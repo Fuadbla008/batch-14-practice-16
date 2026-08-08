20348e5e71e9c22c75407384c9ca00f124cf7fb4
Fuadbla008 <mdbarkatislamfuad@gmail.com>
2026-08-09 03:05:14 +0600

    added full discription

--- describeDeclaration.js
+++ describeDeclaration.js
@@ -1,3 +1,29 @@
+/*
+Scope Detective
+Function Name Must be: describeDeclaration
+একটি কোডিং একাডেমি নতুন শিক্ষার্থীদের var, let, const এর পার্থক্য বোঝাতে একটি ছোট Quiz Tool বানাচ্ছে। Tool-টি একটি keyword ইনপুট নেবে এবং সেই keyword দিয়ে Redeclare ও Reassign করা যায় কিনা তা বর্ণনা করবে।
+Input
+Function একটি Parameter গ্রহণ করবে — keyword (String): "var" / "let" / "const"
+নিয়ম (Rules)
+"var" হলে Return করবে "Can redeclare, can reassign"
+"let" হলে Return করবে "Cannot redeclare, can reassign"
+"const" হলে Return করবে "Cannot redeclare, cannot reassign"
+Validation
+Return "Invalid" যদি keyword এই তিনটির একটিও না হয়।
+টেস্ট কেস
+Input
+Output
+"let"
+Cannot redeclare, can reassign
+"const"
+Cannot redeclare, cannot reassign
+"var"
+Can redeclare, can reassign
+"int"
+Invalid
+*/
+
+
 const describeDeclaration = (input) => {
     if (input === "let") {
         return "Cannot redeclare, can reassign"
@@ -8,7 +34,7 @@ const describeDeclaration = (input) => {
     else if (input === "var") {
         return "Can redeclare, can reassign"
     }
-    else{
+    else {
         return "Invalid"
     }
 }
